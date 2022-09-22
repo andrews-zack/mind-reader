bb9e40256b9d781121dcf391a6aeb6b58fc22ef9
andrews-zack <zachary.andrews.au19@gmail.com>
2022-09-22 05:09:24 +0000

    fixed next button and added functions

--- main.js
+++ main.js
@@ -3,70 +3,98 @@ let nextBtn = document.getElementById("next-Btn");
 let helperText = document.getElementById("helper-Text");
 let bottomBtn = document.getElementById("bottom-Btn");
 
-const newVariable = "new variable";
+
 
 const gameState = {
     screenPage: [
         {
             headerText: "I can read your mind",
-            nextBtn: "none",
+            nextBtn: "NEXT",
             helperText: "",
-            bottomBtn: "GO"
+            bottomBtn: "GO",
+            nextHide: true,
         },
         {
             headerText: "Pick a number from 01-99",
             nextBtn: "NEXT",
             helperText: "When you have your number click NEXT",
-            bottomBtn: "RESET"
+            bottomBtn: "RESET",
+            nextHide: false,
         },
         {
             headerText: "Add both digits together to get a new number",
             nextBtn: "NEXT",
             helperText: "Ex: 14 is 1 + 4 = 5 click NEXT to proceed",
-            bottomBtn: "RESET"
+            bottomBtn: "RESET",
+            nextHide: false,
         },
         {
             headerText: "Subtract your new number from the original number",
             nextBtn: "NEXT",
             helperText: " Ex: 14 - 5 = 9 click next to proceed",
-            bottomBtn: "RESET"
+            bottomBtn: "RESET",
+            nextHide: false,
         },
         {
             headerText: "${randSym}",
             nextBtn: "REVEAL",
             helperText: "Find your new number. Note the symbol beside the number",
-            bottomBtn: "RESET"
+            bottomBtn: "RESET",
+            nextHide: false,
         },
         {
             headerText: "${answerSym}",
-            nextBtn: "none",
+            nextBtn: "REVEAL",
             helperText: "Your symbol is: ${answerSym}",
-            bottomBtn: "RESET"
+            bottomBtn: "RESET",
+            nextHide: true,
         }
     ],
     currentPage: 0,
-    symArr: ["!", "@", "#", "$", "%", "^", "&", "*", "~", "?"],
-    // answerSym: ""
+    symArr: ["!", "@", "#", "$", "%", "^", "&", "*", "~"],
+    answerSym: "${gameState.symArr[0]}"
 };
 
-function initGame() {
-    gameState.currentPage = 0
-    headerText.innerHTML = gameState.screenPage[gameState.currentPage].headerText;
-    nextBtn.style.display = gameState.screenPage[gameState.currentPage].nextBtn;
-    helperText.innerHTML = gameState.screenPage[gameState.currentPage].helperText;
-    bottomBtn.innerHTML = gameState.screenPage[gameState.currentPage].bottomBtn;
-    gameState.currentPage += 1;
+
+
+function scrambleSym(symArr) {
+    let arrLength = gameState.symArr.length;
+    while (arrLength != 0) {
+        randSym = Math.floor(Math.random() * arrLength);
+        arrLength--;
+        [gameState.symArr[arrLength], gameState.symArr[randSym]] = [gameState.symArr[randSym], gameState.symArr[arrLength]];
+    }
+    return symArr;
 }
 
 addEventListener('DOMContentLoaded', initGame);
 
+function initGame() {
+    // gameState.currentPage = 0
+    headerText.innerHTML = gameState.screenPage[gameState.currentPage].headerText;
+    nextBtn.hidden = gameState.screenPage[gameState.currentPage].nextHide;
+    helperText.innerHTML = gameState.screenPage[gameState.currentPage].helperText;
+    bottomBtn.innerHTML = gameState.screenPage[gameState.currentPage].bottomBtn;
+    // gameState.currentPage += 1;
+    bottomBtn.addEventListener("click", updatePage);
+}
+
 function updatePage() {
+    gameState.currentPage += 1;
+    nextBtn.hidden = gameState.screenPage[gameState.currentPage].nextHide;
     headerText.innerHTML = gameState.screenPage[gameState.currentPage].headerText;
     nextBtn.innerHTML = gameState.screenPage[gameState.currentPage].nextBtn;
     helperText.innerHTML = gameState.screenPage[gameState.currentPage].helperText;
     bottomBtn.innerHTML = gameState.screenPage[gameState.currentPage].bottomBtn;
-    gameState.currentPage += 1;
+    bottomBtn.addEventListener("click", resetGame);
+    nextBtn.addEventListener("click", updatePage);
 }
+
+function resetGame() {
+    gameState.currentPage = 0;
+    initGame();
+}
+
 
 /*
 if (bottomBtn.textContent === "GO") {
